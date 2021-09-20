8919f3fa5e52f4e80d06eccc270297c2f924dcc9
Nurendra-bagaskara <73160551+Nurendra-bagaskara@users.noreply.github.com>
2021-09-20 20:24:24 +0700

    Add files via upload

---
+++ home_pelanggan.php
@@ -0,0 +1,13 @@
+<?php 
+    include "header_pelanggan.php";
+
+?>
+<nav class="navbar navbar-expand-lg navbar-dark bg-success" style="box-shadow: 4px 4px 5px -4px;">
+<center>
+<h2>Selamat datang <?=$_SESSION['nama']?> di website Toko Online.</h2>
+</center>
+<?php
+
+    include "footer_pelanggan.php";
+
+?>
